3424681e0aa9ad836f559607cc5444eda6168dc5
Mya Willis <mwillis@pgh.yearup.org>
2022-10-19 14:02:33 -0400

    added assets, style

---
+++ conditional.js
@@ -0,0 +1,9 @@
+let payRate = 25.00;
+let hoursWorked = 40;
+if (hoursWorked <=40) {
+    console.log(`no overtime`);
+    console.log(payRate * hoursWorked);
+} else {
+    (payRate * 1.5) + hoursWorked  
+}
+let grossPay = payRate * hoursWorked;
